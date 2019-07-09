1e8e828c80e67a84f2a5d39008aeb5ab32a86384
SyrahViognier <roger@lewislewiswine.com>
2019-07-09 21:31:41 +0200

    add a login tab copied from register tab

---
+++ client/src/components/Login.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <input
+      type="email"
+      name="email"
+      v-model="email"
+      placeholder="email"
+    />
+    <br/>
+    <input
+      type="password"
+      name="password"
+      v-model="password"
+      placeholder="password"
+    />
+    <br/>
+    <button
+      @click="login"
+    >
+      Login
+    </button>
+  </div>
+</template>
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async login () {
+      await AuthenticationService.login({
+        email: this.email,
+        password: this.password
+      })
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
