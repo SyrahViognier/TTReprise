c818b7fa5cc074e48a56ae4359afe7e472b5d5cb
SyrahViognier <roger@lewislewiswine.com>
2019-07-12 22:34:54 +0200

    add bcrypt and bluebird to hash password

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -9,5 +9,8 @@ module.exports = {
       host: process.env.HOST || 'localhost',
       storage: './ttreprise.sqlite'
     }
+  },
+  authetication: {
+    jwtSecret: process.env.JWT_SECRET || 'secret'
   }
 }
